6fc22a4d8109835b77b315bb921121711149007a
hsalsa <hsalsa@gmail.com>
2013-08-06 09:21:18 -0500

    Maestro de Categoría y Grado

---
+++ Taskool/Controllers/CategoriaController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Taskool.Models;
+
+namespace Taskool.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private TareasEntities db = new TareasEntities();
+
+        //
+        // GET: /Categoria/
+
+        public ActionResult Index()
+        {
+            return View(db.Categoria.ToList());
+        }
+
+        //
+        // GET: /Categoria/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        //
+        // GET: /Categoria/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Categoria/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categoria.Add(categoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        //
+        // GET: /Categoria/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        //
+        // POST: /Categoria/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        //
+        // GET: /Categoria/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        //
+        // POST: /Categoria/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            db.Categoria.Remove(categoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Taskool/Controllers/GradoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Taskool.Models;
+
+namespace Taskool.Controllers
+{
+    public class GradoController : Controller
+    {
+        private TareasEntities db = new TareasEntities();
+
+        //
+        // GET: /Grado/
+
+        public ActionResult Index()
+        {
+            var grado = db.Grado.Include(g => g.Categoria);
+            return View(grado.ToList());
+        }
+
+        //
+        // GET: /Grado/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Grado grado = db.Grado.Find(id);
+            if (grado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grado);
+        }
+
+        //
+        // GET: /Grado/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.idCategoria = new SelectList(db.Categoria, "id", "nombreCategoria");
+            return View();
+        }
+
+        //
+        // POST: /Grado/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Grado grado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grado.Add(grado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCategoria = new SelectList(db.Categoria, "id", "nombreCategoria", grado.idCategoria);
+            return View(grado);
+        }
+
+        //
+        // GET: /Grado/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Grado grado = db.Grado.Find(id);
+            if (grado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCategoria = new SelectList(db.Categoria, "id", "nombreCategoria", grado.idCategoria);
+            return View(grado);
+        }
+
+        //
+        // POST: /Grado/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Grado grado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(grado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCategoria = new SelectList(db.Categoria, "id", "nombreCategoria", grado.idCategoria);
+            return View(grado);
+        }
+
+        //
+        // GET: /Grado/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Grado grado = db.Grado.Find(id);
+            if (grado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grado);
+        }
+
+        //
+        // POST: /Grado/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Grado grado = db.Grado.Find(id);
+            db.Grado.Remove(grado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
